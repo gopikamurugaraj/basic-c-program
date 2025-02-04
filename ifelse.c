9bdf134eb92c0d4c8a9f66e9ed67cc881c9bbb53
gopikamurugaraj <vmgopikamurugaraj@gmail.com>
2025-02-04 14:20:19 +0530

    initial commit

---
+++ ifelse.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() {
+int age;
+printf("Enter the age:");
+scanf("%d",&age);
+if(age<12){
+    printf("they are a child and have 50 percent discount");
+}
+if(age>=12 && age<=18){
+    printf("they are teenagers:");
+    if(age==18){
+        printf("they have 20 percent discount");
+    }
+    else
+       {
+           printf("they have 10 percent");
+       }
+    
+}
+if(age>18){
+    printf("They are adult:");
+
+if (age==60 || age>60){
+    printf("they are senior citizen and have 30 percent");
+}
+else{
+    printf("they get no discount");
+}
+    
+}
+      return 0;
+
+}
