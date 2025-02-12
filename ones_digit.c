6fe0ee10eeafea330096c61cb43b9f1ff152c4b3
gopikamurugaraj <vmgopikamurugaraj@gmail.com>
2025-02-12 14:41:09 +0530

    updated code

---
+++ ones_digit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int num1,rem;
+    printf("enter the num1:");
+    scanf("%d",&num1);
+
+   rem=num1%10;
+    printf("The one's digit of the number is :%d",rem);
+
+}
