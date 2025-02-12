2ed5cf617c5fd6c1a5076cac426e5e2eb2aaf7da
gopikamurugaraj <vmgopikamurugaraj@gmail.com>
2025-02-12 14:17:49 +0530

    updated code

--- div_rem.c
+++ div_rem.c
@@ -3,7 +3,8 @@ int main(){
     int num1,divison;
     printf("enter the num1:");
     scanf("%d",&num1);
-   divison=num1/5;
-    printf("The division of %d and 6 is:%d",num1,divison);
+   divison=num1%8;
+    printf("The division of %d and 8 is:%d",num1,divison);
 
 }
+
